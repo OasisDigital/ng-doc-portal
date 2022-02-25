204edf64c5ce371b929013c2563e385767a00052
Zachary Kipping <zjkipping@hotmail.com>
2022-02-25 14:13:12 -0600

    minor cleanup from workspace creation

--- apps/component-document-portal/src/app/app.module.ts
+++ apps/component-document-portal/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
