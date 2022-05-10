11652149b7a0c03b7b379a68a076b49cca4f8c8c
Alisha Robinson <alisha.robinson@oasisdigital.com>
2022-05-10 16:23:48 +0000

    Merged in feature/CARD4-Schematic-for-Doc-Pages (pull request #10)
    Feature/CARD4 Schematic for Doc Pages
    
    * schematic for doc pages
    
    
    Approved-by: Zachary Kipping

---
+++ tools/generators/doc-page/index.ts
@@ -0,0 +1,36 @@
+import {
+  Tree,
+  logger,
+  names,
+  generateFiles,
+  formatFiles,
+  getProjects,
+  readWorkspaceConfiguration,
+} from '@nrwl/devkit';
+
+export interface DocPageSchema {
+  name: string;
+  path?: string;
+}
+const execSchematicCommand = `npm run nx workspace-generator doc-page`;
+
+export default async function (tree: Tree, schema: DocPageSchema) {
+  logger.info(
+    `✳️  Starting Schematic Command: ✳️  ${execSchematicCommand} ${schema.name}`
+  );
+  const defaultProject = readWorkspaceConfiguration(tree).defaultProject;
+  const findPath = getProjects(tree).get(defaultProject ?? '')?.root;
+  const targetPath = findPath ?? './';
+  const templatePath = 'tools/generators/doc-page/templates';
+  const interfaceNames = names(schema.name);
+
+  const substitutions = {
+    path: schema.path ? schema.path + '/' : '',
+    // remove __tmpl__ from file endings
+    tmpl: '',
+    // make the different name variants available as substitutions
+    ...interfaceNames,
+  };
+  generateFiles(tree, templatePath, targetPath, substitutions);
+  await formatFiles(tree);
+}
