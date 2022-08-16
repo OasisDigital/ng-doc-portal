2b93934ba4c4498a3500bdd755a8d65e56773c12
Zachary Kipping <zjkipping@hotmail.com>
2022-08-16 16:15:09 -0500

    Fix code reveal copy button padding, rev to 0.3.2

--- apps/component-document-portal/src/app/doc-pages/input.doc-page.ts
+++ apps/component-document-portal/src/app/doc-pages/input.doc-page.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-import { DocPageConfig } from '@oasisdigital/ng-doc-portal';
+import {
+  DocPageConfig,
+  NgDocPortalComponentsModule,
+} from '@oasisdigital/ng-doc-portal';
 
 @Component({
   standalone: true,
+  imports: [NgDocPortalComponentsModule],
   template: `
     <h1>Input Component Document Page</h1>
     <input placeholder="Example Input" />
+
+    <cdp-code-reveal lang="html">
+      <label>Hello World</label>
+      <input placeholder="Example Input" />
+      <button>Example Button</button>
+    </cdp-code-reveal>
   `,
 })
 export class InputDocumentPageComponent {}
